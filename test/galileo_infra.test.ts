48f4caf63e8d67161f4ab5c636b352ae363bf23a
Olerato Masiu <52298509+ortmasiu@users.noreply.github.com>
2023-05-17 14:16:45 +0200

    Add files via upload

---
+++ test/galileo_infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GalileoInfra from '../lib/galileo_infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/galileo_infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GalileoInfra.GalileoInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
